faf24160f42e09387a1513854b230777a05c729e
Abijith Kp <abijith.akp@gmail.com>
2016-10-16 20:04:20 +0530

    Added feature: Progressivly increasing the list of links.

--- links/links.py
+++ links/links.py
@@ -2,11 +2,11 @@ from flask import jsonify, render_template, request, flash, redirect, url_for, j
 from datetime import datetime
 
 from apps import links_app
-from models import *
+from models import Links
 from . import db
 
 #session = create_session()
-session = create_session("sqlite:///proto.db")
+# session = create_session("sqlite:///proto.db")
 PAGE_SIZE = 10
 
 @links_app.route("/", methods=["GET"])
@@ -19,8 +19,8 @@ def analytics():
 
 @links_app.route("/del/<string:uuid>", methods=["GET"])
 def delete_link(uuid):
-    l = session.query(Links).filter_by(uuid=uuid).delete()
-    session.commit()
+    l = Links.query.filter_by(uuid=uuid).delete()
+    db.session.commit()
     print l
     return str(l)
 
@@ -28,23 +28,19 @@ def delete_link(uuid):
 def update_link(uuid):
     args = request.args
     print args
-    result = session.query(Links).all()
-    l = session.query(Links).filter_by(uuid=uuid).first()
+    result = Links.query.all()
+    l = Links.query.filter_by(uuid=uuid).first()
     l.title = args.get('title', l.title)
-    session.commit()
+    db.session.commit()
     print l, l.link, l.title
     return ""
 
 @links_app.route("/url", methods=["POST", "GET"])
 def url():
-    #args = request.args
-    #print args
-    #page = int(args.get('page', 1))
-    # result = session.query(Links).all()
-    #result = session.query(Links).limit(PAGE_SIZE).offset((page - 1) * PAGE_SIZE)
-    result = session.query(Links.uuid, Links.link, Links.title).all()
-    # print jsonify([{"link":r.link, "updated":datetime.strftime(r.updated, "%a, %d %b %Y %X +%f")}    for r in result])
-    #print [{"link":r.link, "title":r.title}    for r in result]
+    args = request.args
+    print args
+    page = int(args.get('page', 1))
+    result = Links.query.limit(PAGE_SIZE).offset((page - 1) * PAGE_SIZE)
+    #result = Links.query.all()
     print "selected all the table"
     return jsonify({r.uuid:[r.link, r.title]    for r in result})
-    #return jsonify({r.link:datetime.strftime(r.updated, "%a, %d %b %Y %X +%f")    for r in result})
--- links/models/models.py
+++ links/models/models.py
@@ -3,9 +3,11 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
+from .. import db
 
-class Links(Base):
+# Base = declarative_base()
+
+class Links(db.Model):
     __tablename__ = 'links'
 
     id          =   Column(Integer, primary_key=True)
@@ -14,13 +16,28 @@ class Links(Base):
     title        =   Column(String(2048))
     uuid        =   Column(String(32), unique=True, nullable=False)
 
-class Config(Base):
+    def __init__(self, link, updated, title, uuid):
+        self.link = link
+        self.updated = updated
+        self.title = title
+        self.uuid = uuid
+
+
+    def __repr__(self):
+        return "<Links %s %s %s>" % (self.uuid, self.link, self.title)
+
+class Config(db.Model):
     __tablename__ = "config"
 
     id = Column(Integer, primary_key=True)
     last_updated = Column(DateTime, nullable=False)
     last_updated_after = Column(DateTime)
     last_updated_before = Column(DateTime)
+
+    def __init__(self, last_updated, last_updated_before, last_updated_after):
+        self.last_updated = last_updated
+        self.last_updated_before = last_updated_before
+        self.last_updated_after = last_updated_after
 
 def create_session(db='sqlite:///proto.db'):
     engine = create_engine(db, echo=False)
